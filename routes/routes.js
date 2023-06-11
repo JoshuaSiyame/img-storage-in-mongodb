1ce66bdfab84baff7322c24a570e6afb72cfb690
joshcodingenv <josh2023decoding@gmail.com>
2023-06-11 09:15:41 +0300

    routes fix

--- routes/routes.js
+++ routes/routes.js
@@ -16,5 +16,25 @@ router.get("/test", (req, res)=>{
     res.status(200).send("Working");
 });
 
+router.get("/upload", (req, res)=>{
+    res.render("pages/upload");
+});
+
+router.post("/upload", upload.single("image"), async (req, res)=>{
+    try {
+        // get submitted data
+        const { name, description } = req.body;
+
+        // file uploaded
+        const file = req.file;
+
+        console.table([name, description, file]);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Something broke");
+    };
+});
+
 // export router instance
 module.exports = router;
